bc569bb42505b89e300f33316f3a05877c13a379
vuong1202 <95081875+vuong1202@users.noreply.github.com>
2021-12-11 19:56:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Vượng Cute",
+    introTitle: 'Mỡ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Phải chăng em bị ngu si 🥰',
+    desc: 'Hay là bị ngáo ta ',
+    btnYes: 'Có chứ <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
