68e62ed9a9d3bb2cc0c7308ee0842ba045ec2e14
vuong1202 <95081875+vuong1202@users.noreply.github.com>
2021-12-11 20:31:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 const CONFIG = {
     titleWeb: "Vượng Cute",
     introTitle: 'Anh hỏi cái này',
-    introDesc: `Đây là một câu hỏi dễ thui
-    Trẻ con cũng biết
+    introDesc: `Đây là một câu hỏi dễ thui.
+    
+    Trẻ con cũng biết.
+    
     Không biết thì ngu si lắm`,
     btnIntro: '^^HiHi^^',
-    title: 'Em có bị ngu si không? 🥰',
+    title: 'Em có bị ngu si không?🤔',
     desc: 'Dễ nhỉ?? Trả lời nhanh nhanh lên xem nào ',
     btnYes: 'Có chứ <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Không nha 🙂',
     question: 'Haha. Có ý kiến gì không?? Ghi vào đây nè <3',
     btnReply: 'Gửi đi <3',
-    reply: 'Ơ. Bị em ngu thật mà !  :)))))',
-    mess: 'Anh biết ngay mà :). Không sao tuy ngu nhưng được cái xấu xí',
+    reply: 'Ơ. Em bị ngu thật mà. Không chỉ vậy còn bị ngáo nữa :)))))',
+    mess: 'Anh biết ngay mà 🤣. Không sao tuy ngu nhưng được cái xấu xí 🙂',
     messDesc: 'Vậy là quá xịn. Nhỉ ',
-    btnAccept: 'Okiiiii :))',
+    btnAccept: 'Okiiiii :)',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
